765f61d984af3e9e84dce6231cfeb57118943a06
Nadya Pagiling <S22310566@student.unklab.ac.id>
2024-11-12 02:26:50 -0800

    adding header

---
+++ project_portfolio/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header-content">
+                <h1>Nadya Sikami Pagiling</h1>
+                <p>Mahaasiswa | Informatika/Unklab</p>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
