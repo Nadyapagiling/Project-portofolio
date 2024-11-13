8e92e2ef903d5c8ebbd3342994d4aed843613862
Nadya Pagiling <S22310566@student.unklab.ac.id>
2024-11-13 04:30:40 -0800

    adding portofolio

--- project_portfolio/src/App.js
+++ project_portfolio/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import About from './components/About';
 import Experience from './components/Experience';
+import Portfolio from './components/Portfolio';
 import './App.css'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <main>
         <About />
         <Experience />
+        <Portfolio />
       </main>
     </div>
   );
---
+++ project_portfolio/src/components/Portofolio.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Portfolio = () => {
+    return (
+        <section id="portfolio">
+            <h2>Portofolio</h2>
+            <ul>
+                <li><a href="link-ke-proyek1">Proyek 1</a></li>
+                <li><a href="link-ke-proyek2">Proyek 2</a></li>
+                <li><a href="link-ke-proyek3">Proyek 3</a></li>
+            </ul>
+        </section>
+    );
+}
+
+export default Portfolio;
