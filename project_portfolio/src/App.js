c308dceaf47c9f6dc8f134218c48116dcab0461e
Nadya Pagiling <S22310566@student.unklab.ac.id>
2024-11-13 04:21:31 -0800

    adding About

--- project_portfolio/src/App.js
+++ project_portfolio/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
+import About from './components/About';
+import './App.css'
 
 function App() {
   return (
-    <div >
+    <div>
       <Header />
+      <main>
+        <About />
+      </main>
     </div>
   );
-}
+};
 
 export default App;
---
+++ project_portfolio/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <section>
+            <h2>Tentang saya</h2>
+            <p>Denskripsi</p>
+        </section>
+    );
+}
+
+export default About;
