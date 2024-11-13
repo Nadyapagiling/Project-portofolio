0a7d0dade2f5c883ed806893ecf07b3920760f02
Nadya Pagiling <S22310566@student.unklab.ac.id>
2024-11-13 04:27:25 -0800

    adding experience

--- project_portfolio/src/App.js
+++ project_portfolio/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import About from './components/About';
+import Experience from './components/Experience';
 import './App.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <main>
         <About />
+        <Experience />
       </main>
     </div>
   );
---
+++ project_portfolio/src/components/Experience.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Experience = () => {
+    return (
+        <section id="experience">
+            <h2>Pengalaman Kerja</h2>
+            <ul>
+                <li>Posisi 1 - Perusahaan 1 (Tahun)</li>
+                <li>Posisi 2 - Perusahaan 2 (Tahun)</li>
+                <li>Posisi 3 - Perusahaan 3 (Tahun)</li>
+            </ul>
+        </section>
+    );
+}
+
+export default Experience;
