9dd66861fd2a411db915b683a4a48b6e556e54be
Nadya Pagiling <S22310566@student.unklab.ac.id>
2024-11-14 06:09:15 -0800

    menambahkan about jsnya

--- project_portfolio/src/components/About.js
+++ project_portfolio/src/components/About.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const About = () => {
     return (
-        <section>
-            <h2>Tentang saya</h2>
-            <p>Denskripsi</p>
+        <section id="about">
+            <h1>A little about me</h1>
+            <p>Saya adalah seseorang yang selalu tertarik untuk mengeksplorasi kreativitas dan imajinasi tanpa batas. Dengan latar belakang di bidang Informatika, saya percaya bahwa perpaduan antara seni dan teknologi dapat menghasilkan sesuatu yang luar biasa. Saya senang mendalami berbagai proyek, baik itu dalam fotografi, desain web, atau seni 3D, untuk menciptakan karya yang bermakna dan inovatif. Setiap pengalaman baru memberikan inspirasi untuk melihat dunia dari sudut pandang berbeda, dan melalui karya-karya saya, saya berusaha untuk menghidupkan ide-ide tersebut. Terima kasih telah berkunjung, dan saya berharap dapat berbagi inspirasi bersama Anda.</p>
         </section>
     );
 }
