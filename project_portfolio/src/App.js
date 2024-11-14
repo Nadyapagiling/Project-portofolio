43f83ed9512b3be99ac7688c57286755d8a288b6
Nadya Pagiling <S22310566@student.unklab.ac.id>
2024-11-14 06:29:41 -0800

    adding Blog

--- project_portfolio/src/App.js
+++ project_portfolio/src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import About from './components/About';
 import Services from './components/Services';
 import Portofolio from './components/Portofolio';
+import Blog from './components/Blog';
 import './App.css'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <About />
       <Services />
       <Portofolio />
+      <Blog />
     </div>
   );
 };
---
+++ project_portfolio/src/components/Blog.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Blog = () => {
+    return (
+        <section id="blog">
+        <h1>My little blog</h1>
+        <div>
+            <h2>Creative Minds</h2>
+            <p>Di balik setiap inovasi besar, ada pikiran-pikiran kreatif yang berani berpikir di luar batasan. *Creative Minds* adalah tentang keberanian untuk mengeksplorasi ide-ide baru, membongkar pola lama, dan membangun sesuatu yang tak terbayangkan. Dengan kreativitas dan ketajaman pikiran, kita dapat menciptakan solusi yang tidak hanya efektif tetapi juga menginspirasi. Mari bersama-sama menjelajahi bagaimana kekuatan berpikir kreatif bisa mengubah dunia.</p>
+            <h2>Creative Hearts</h2>
+            <p>Sebuah karya yang penuh arti lahir dari hati yang penuh inspirasi. *Creative Hearts* adalah tentang semangat yang mendorong kita untuk menciptakan sesuatu yang lebih dari sekadar bentuk dan fungsi. Kreativitas yang berasal dari hati menciptakan karya yang menyentuh, yang memiliki makna dan keindahan tersendiri. Mari bersama, kita membangun sesuatu yang abadi, menciptakan dari hati, dan menghadirkan kehangatan dalam setiap karya kita.</p>
+        </div>
+    </section>
+    );
+};
+
+export default Blog;
