25f28f51be3a0f97559aa3bf5d94330fe1994706
Nadya Pagiling <S22310566@student.unklab.ac.id>
2024-11-14 06:23:12 -0800

    menambahkan yang kurang di protofolio

--- project_portfolio/src/components/Portofolio.js
+++ project_portfolio/src/components/Portofolio.js
@@ -3,12 +3,11 @@ import React from 'react';
 const Portofolio = () => {
     return (
         <section id="portofolio">
-            <h2>Portofolio</h2>
-            <ul>
-                <li><a href="link-ke-proyek1">Proyek 1</a></li>
-                <li><a href="link-ke-proyek2">Proyek 2</a></li>
-                <li><a href="link-ke-proyek3">Proyek 3</a></li>
-            </ul>
+            <h1>Recent Projects</h1>
+            <div>
+                <img src="path/to/image1.jpg" alt="Project 1" />
+                <img src="path/to/image2.jpg" alt="Project 2" />
+            </div>
         </section>
     );
 }
